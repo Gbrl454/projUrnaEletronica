8e742611d1c1220e3fa3716f865c3124c5ff5da2
Gabriel Matos <gabrielmatos@unifor.br>
2022-10-19 18:43:08 -0300

    Banco de Dados AWS

--- src/main/java/gbrl/ue/database/InfoDB.java
+++ src/main/java/gbrl/ue/database/InfoDB.java
@@ -2,11 +2,11 @@ package gbrl.ue.database;
 
 public interface InfoDB {
 
-    String IP = "localhost";
+    String IP = "projetournaeletronicapoo.c96lvttyochh.sa-east-1.rds.amazonaws.com";
     String BANCO = "urnaeletronica";
 
-    String USER = "root";
-    String SENHA = "";
+    String USER = "purnaeletronicap";
+    String SENHA = "ue55747322poo";
 
     String urlBD = "jdbc:mysql://" + IP + "/" + BANCO + "?autoReconnect=true&useSSL=false&user=" + USER + "&password=" + SENHA;
 
